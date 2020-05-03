8ab5d8f3aa1812002160781087ec7f8edc7009ca
jahlgren <jukka.ahlgren@gmail.com>
2020-05-03 11:15:07 +0300

    Web-palvelinohjelmointi Java 2020

---
+++ README.md
@@ -0,0 +1,12 @@
+# Web-palvelinohjelmointi Java 2020
+
+Ohjelmointiprojekti, jossa luodaan ansioluettelo- ja työntekijähakusovellus eli tuttavallisemmin vanhan kansan LinkedIn.
+
+[![CircleCI](https://circleci.com/gh/it-innoo/linkedin.svg?style=svg)](https://circleci.com/gh/it-innoo/linkedin)
+
+
+
+[![codecov](https://codecov.io/gh/it-innoo/linkedin/branch/master/graph/badge.svg)](https://codecov.io/gh/it-innoo/linkedin)
+
+<a href="https://codeclimate.com/github/it-innoo/linkedin/maintainability"><img src="https://api.codeclimate.com/v1/badges/ed133446a76af6d4dc6c/maintainability" /></a>
+
---
+++ src/test/java/projekti/DefaultControllerTest.java
@@ -0,0 +1,28 @@
+
+package projekti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@WebMvcTest(DefaultController.class)
+public class DefaultControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        mvc.perform(get("/")
+                .contentType(MediaType.TEXT_HTML))
+                .andExpect(status().isOk());
+    }
+}
