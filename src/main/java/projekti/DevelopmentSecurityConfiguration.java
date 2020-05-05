10572fce1428091b1f055b203cdbf2b358e0e801
jahlgren <jukka.ahlgren@gmail.com>
2020-05-05 16:52:49 +0300

    Login part 2

--- src/main/java/projekti/DevelopmentSecurityConfiguration.java
+++ src/main/java/projekti/DevelopmentSecurityConfiguration.java
@@ -1,15 +1,45 @@
 package projekti;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class DevelopmentSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+/*
     @Override
     public void configure(WebSecurity sec) throws Exception {
         // Pyyntöjä ei tarkasteta
         sec.ignoring().antMatchers("/**");
+    }
+*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated().and()
+                .formLogin().permitAll().and()
+                .logout().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+          .withUser("jukka").password(passwordEncoder().encode("salainen")).roles("USER")
+          .and()
+          .withUser("ukko").password(passwordEncoder().encode("salainen")).roles("USER")
+          .and()
+          .withUser("admin").password(passwordEncoder().encode("sekret")).roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/test/java/projekti/DefaultControllerTest.java
+++ src/test/java/projekti/DefaultControllerTest.java
@@ -1,4 +1,3 @@
-
 package projekti;
 
 import org.junit.Test;
@@ -6,21 +5,30 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @WebMvcTest(DefaultController.class)
 public class DefaultControllerTest {
+
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void testHelloWorld() throws Exception {
+    public void requestProtectedResourceRequiresAuthentication() throws Exception {
+        mvc.perform(get("/")).andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    @WithMockUser(value = "ukko")
+    public void requestProtectedResourceWithUser() throws Exception {
         mvc.perform(get("/")
                 .contentType(MediaType.TEXT_HTML))
                 .andExpect(status().isOk());
